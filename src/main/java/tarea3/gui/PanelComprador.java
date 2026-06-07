7e109c7d5615a77a73dd12cf1ac870f182f663d8
NicoFSPD <linegarcina@gmail.com>
2026-06-07 15:17:34 -0400

    feat/docs: Agregar los objetos que son imagenes que la clase va a utilizar mas el metodo que los carga

--- src/main/java/tarea3/gui/PanelComprador.java
+++ src/main/java/tarea3/gui/PanelComprador.java
@@ -1,8 +1,9 @@
 package tarea3.gui;
 
-import javax.swing.JPanel;
-import java.awt.Color;
-import java.awt.Graphics;
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
 import tarea3.logica.Expendedor;
 
 /**
@@ -18,6 +19,18 @@ public class PanelComprador extends JPanel {
     private Expendedor exp;
     private PanelPrincipal parent;
 
+    // Objetos Image para almacenar las texturas cargadas
+    private Image imgCoca;
+    private Image imgSprite;
+    private Image imgFanta;
+    private Image imgSnickers;
+    private Image imgSuper8;
+    // ATRIBUTOS NUEVOS PARA LAS MONEDAS
+    private Image imgM100;
+    private Image imgM500;
+    private Image imgM1000;
+    private Image imgM1500;
+
     /**Constructor de la clase PanelComprador.
      * @param exp el modelo logico de la maquina expendedora.
      * @param parent referencia del panel principal para poder actualizarce en la pantalla.
@@ -26,6 +39,28 @@ public class PanelComprador extends JPanel {
         this.exp = exp;
         this.parent = parent;
     }
+
+    /**
+     * Carga de forma segura los archivos de imagen desde la carpeta de recursos de Maven.
+     */
+    private void cargarImagenes() {
+        try {
+            imgCoca = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/cocacola.png"))).getImage();
+            imgSprite = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/sprite.png"))).getImage();
+            imgFanta = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/fanta.png"))).getImage();
+            imgSnickers = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/snickers.png"))).getImage();
+            imgSuper8 = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/super8.png"))).getImage();
+            // CARGA DE LAS NUEVAS TEXTURAS DE MONEDAS
+            imgM100 = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/moneda100.png"))).getImage();
+            imgM500 = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/moneda500.png"))).getImage();
+            imgM1000 = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/moneda1000.png"))).getImage();
+            imgM1500 = new ImageIcon(Objects.requireNonNull(getClass().getResource("/images/moneda1500.png"))).getImage();
+        } catch (Exception e) {
+            System.out.println("Error critico al cargar las imagenes del expendedor: " + e.getMessage());
+        }
+    }
+
+
 
     /**Procesa y evalua los clicks del mouse realizados dentro de la region del comprador.
      * Permite al comprador seleccionar productos, escoger monedas para el pago o retirar productos.
